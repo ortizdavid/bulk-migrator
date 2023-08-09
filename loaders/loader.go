467b7f4279d10c387eb27c7f1a0c82f834634ea5
Ortiz de Arcanjo Antonio David <ortizaad1994@gmail.com>
2023-08-09 15:02:48 +0100

    Preparing LoadDatabase function

--- loaders/loader.go
+++ loaders/loader.go
@@ -5,6 +5,10 @@ import "github.com/ortizdavid/bulk-migrator/decoders"
 type Loader struct {
 }
 
-func (loader *Loader) LoadDatabase(source decoders.DatabaseConfig, target decoders.DatabaseConfig) {
-
+func (loader *Loader) LoadDatabase(sourceConfig string, targetConfig string) {
+	var dbConfig decoders.DatabaseConfig
+	source := dbConfig.DecodeDbConfig(sourceConfig)
+	target := dbConfig.DecodeDbConfig(targetConfig)
+	source.	PrintConfigs()
+	target.PrintConfigs()
 }
